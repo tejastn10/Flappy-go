04af4af074fcf4a14a525123bb4056be83adb8a6
tejastn10 <tejastn10@gmail.com>
2020-03-16 00:47:25 +0530

    Bird animated

--- scene.go
+++ scene.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/veandco/go-sdl2/img"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
 type scene struct {
-	bg   *sdl.Texture
-	bird *sdl.Texture
+	time int
+
+	bg    *sdl.Texture
+	birds []*sdl.Texture
 }
 
 func newScene(r *sdl.Renderer) (*scene, error) {
@@ -18,15 +22,41 @@ func newScene(r *sdl.Renderer) (*scene, error) {
 		return nil, fmt.Errorf("could not load background: %v", err)
 	}
 
-	bird, err := img.LoadTexture(r, "assets/images/bird_frame_1.png")
-	if err != nil {
-		return nil, fmt.Errorf("could not load background: %v", err)
+	var birds []*sdl.Texture
+	for i := 1; i <= 4; i++ {
+		path := fmt.Sprintf("assets/images/bird_frame_%d.png", i)
+		bird, err := img.LoadTexture(r, path)
+		if err != nil {
+			return nil, fmt.Errorf("could not load background: %v", err)
+		}
+		birds = append(birds, bird)
 	}
 
-	return &scene{bg: bg, bird: bird}, nil
+	return &scene{bg: bg, birds: birds}, nil
+}
+
+func (s *scene) run(ctx context.Context, r *sdl.Renderer) <-chan error {
+	errc := make(chan error)
+
+	go func() {
+		defer close(errc)
+		for range time.Tick(100 * time.Millisecond) {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if err := s.paint(r); err != nil {
+					errc <- err
+				}
+			}
+		}
+	}()
+	return errc
 }
 
 func (s *scene) paint(r *sdl.Renderer) error {
+	s.time++
+
 	r.Clear()
 
 	if err := r.Copy(s.bg, nil, nil); err != nil {
@@ -34,7 +64,9 @@ func (s *scene) paint(r *sdl.Renderer) error {
 	}
 
 	rect := &sdl.Rect{X: 10, Y: 300 - 43/2, W: 50, H: 43}
-	if err := r.Copy(s.bird, nil, rect); err != nil {
+
+	i := s.time % len(s.birds)
+	if err := r.Copy(s.birds[i], nil, rect); err != nil {
 		return fmt.Errorf("could not copy background: %v", err)
 	}
 
