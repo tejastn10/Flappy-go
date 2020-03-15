15a8374ac6db4356634b4b19940d4a6895bc820c
tejastn10 <tejastn10@gmail.com>
2020-03-15 23:44:15 +0530

    Bird added

--- scene.go
+++ scene.go
@@ -8,22 +8,33 @@ import (
 )
 
 type scene struct {
-	bg *sdl.Texture
+	bg   *sdl.Texture
+	bird *sdl.Texture
 }
 
 func newScene(r *sdl.Renderer) (*scene, error) {
-	t, err := img.LoadTexture(r, "assets/images/background.png")
+	bg, err := img.LoadTexture(r, "assets/images/background.png")
 	if err != nil {
 		return nil, fmt.Errorf("could not load background: %v", err)
 	}
 
-	return &scene{bg: t}, nil
+	bird, err := img.LoadTexture(r, "assets/images/bird_frame_1.png")
+	if err != nil {
+		return nil, fmt.Errorf("could not load background: %v", err)
+	}
+
+	return &scene{bg: bg, bird: bird}, nil
 }
 
 func (s *scene) paint(r *sdl.Renderer) error {
 	r.Clear()
 
 	if err := r.Copy(s.bg, nil, nil); err != nil {
+		return fmt.Errorf("could not copy background: %v", err)
+	}
+
+	rect := &sdl.Rect{X: 10, Y: 300 - 43/2, W: 50, H: 43}
+	if err := r.Copy(s.bird, nil, rect); err != nil {
 		return fmt.Errorf("could not copy background: %v", err)
 	}
 
